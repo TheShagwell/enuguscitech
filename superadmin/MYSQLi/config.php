a5bf31afa2e4a4efc2b88b955f10cd5d4e6bd103
godson-pius <40927597+godson-pius@users.noreply.github.com>
2022-04-13 14:36:58 +0100

    Server Config

--- superadmin/MYSQLi/config.php
+++ superadmin/MYSQLi/config.php
@@ -1,4 +1,5 @@
 <?php
 
 require_once('wbtml.php');
-SETUP("localhost", "root", "", "enuguscitech");
+// SETUP("localhost", "root", "", "enuguscitech");
+SETUP("localhost", "worlgmig_enugu", "100%enugu", "worlgmig_enugutest");
